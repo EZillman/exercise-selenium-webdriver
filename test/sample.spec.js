95dc982737eee0001eaebd56cc1ce0314cc3f6c4
EZillman <zillmanevelina@gmail.com>
2025-01-17 16:21:38 +0100

    [added] submit firstname script

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -3,12 +3,22 @@ const assert = require('assert');
 
 describe('Open Edge', function () {
     let driver;
+    
     before(async function () {
         driver = await new Builder().forBrowser(Browser.EDGE).build();
     });
 
     it('Open samplesite in Edge', async function () {
         await driver.get('https://artoftesting.com/samplesiteforselenium');
+        await driver.manage().setTimeouts({implicit: 500});
+    });
+
+    it('Submit firstname', async function () {
+        let firstNameInput = await driver.findElement(By.name('firstName'));
+        let submitButton = await driver.findElement(By.id('idOfButton'));
+
+        await firstNameInput.sendKeys('Jane');
+        await submitButton.click();
     });
 
     after(async function () {
